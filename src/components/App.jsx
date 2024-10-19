3729b0d3aa77cdaa76e20ee4a4564caeecdff00c
ayushmehan <mehan.ayush16@gmail.com>
2024-10-19 14:28:40 +0530

    first-coomit

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+ 
+import Home from "../page/Home";
+import Room from "../page/Room";
+import Join from "../page/Join.jsx";
+import NotFound from "../page/NotFound";
+import Scanqr from "../page/Scanqr.jsx";
+import NewRoom from "../page/NewRoom";
+
+const App = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="max-h-screen overflow-auto w-full">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/room/:roomID" element={<NewRoom />} /> */}
+          <Route path="/room/:roomID" element={<Room />} />
+          <Route path="/join" element={<Join />} />
+          <Route path="/scanqr" element={<Scanqr />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
